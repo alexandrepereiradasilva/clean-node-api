56dd2d546617157af9db76336455cd79cc53ce01
alexandrepereiradasilva <alexandrepereiradasilva@users.noreply.github.com>
2020-12-25 10:44:35 -0300

    ensure EmailValidator calls validator with correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -4,17 +4,28 @@ class EmailValidator {
     return validator.isEmail(email)
   }
 }
+
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('Should return true if validator returns true', () => {
-    const stu = new EmailValidator()
+    const stu = makeSut()
     const isEmailValid = stu.isValid('valid_email@gmail.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
-    const stu = new EmailValidator()
+    const stu = makeSut()
     const isEmailValid = stu.isValid('invalid_email@gmail.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const stu = makeSut()
+    stu.isValid('any_email@gmail.com')
+    expect(validator.email).toBe('any_email@gmail.com')
   })
 })
