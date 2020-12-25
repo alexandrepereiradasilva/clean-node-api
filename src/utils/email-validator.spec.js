42741f5f55053cb7bcb8661a153d87300fadab09
alexandrepereiradasilva <alexandrepereiradasilva@users.noreply.github.com>
2020-12-25 10:40:08 -0300

    ensure EmailValidator

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -8,12 +8,13 @@ describe('Email Validator', () => {
   test('Should return true if validator returns true', () => {
     const stu = new EmailValidator()
     const isEmailValid = stu.isValid('valid_email@gmail.com')
-    exports(isEmailValid).toBe(true)
+    expect(isEmailValid).toBe(true)
   })
 
   test('Should return false if validator returns false', () => {
+    validator.isEmailValid = false
     const stu = new EmailValidator()
-    const isEmailValid = stu.isValid('invalid_email')
-    exports(isEmailValid).toBe(false)
+    const isEmailValid = stu.isValid('invalid_email@gmail.com')
+    expect(isEmailValid).toBe(false)
   })
 })
